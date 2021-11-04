f6f6b9afa41da50641a57b99a5143e9e8a5fb536
Matan_Buljubasic <mbuljubasic@etfos.hr>
2021-11-04 12:06:03 +0100

    Added DZ1.

---
+++ DZ1/Zadaca1/Constants.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classlibrary
+{
+    class Constants
+    {
+        private const double c1 = -8.78469475556;
+        private const double c2 = 1.61139411;
+        private const double c3 = 2.33854883889;
+        private const double c4 = -0.14611605;
+        private const double c5 = -0.012308094;
+        private const double c6 = -0.0164248277778;
+        private const double c7 = 0.002211732;
+        private const double c8 = 0.00072546;
+        private const double c9 = -0.000003582;
+
+        public double GetC1() { return c1; }
+        
+        public double GetC2() { return c2; }
+
+        public double GetC3() { return c3; }
+
+        public double GetC4() { return c4; }
+
+        public double GetC5() { return c5; }
+
+        public double GetC6() { return c6; }
+
+        public double GetC7() { return c7; }
+
+        public double GetC8() { return c8; }
+
+        public double GetC9() { return c9; }
+    }
+}
---
+++ DZ1/Zadaca1/Weather.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classlibrary
+{
+    
+    public class Weather
+    {
+        private double temperature;
+        private double humidity;
+        private double windSpeed;
+
+        public Weather() { }
+
+        public Weather(double temperature, double humidity, double windSpeed) { this.temperature = temperature; this.humidity = humidity; this.windSpeed = windSpeed; }
+
+        public void SetTemperature(double temperature) { this.temperature = temperature; }
+
+        public void SetHumidity(double humidity) { this.humidity = humidity; }
+
+        public void SetWindSpeed(double windSpeed) { this.windSpeed = windSpeed; }
+
+        public double GetTemperature() { return temperature; }
+
+        public double GetHumidity() { return humidity; }
+
+        public double GetWindSpeed() { return windSpeed; }
+
+        public double CalculateFeelsLikeTemperature() 
+        {   Constants constants = new Constants();
+            return constants.GetC1() + constants.GetC2() * temperature + constants.GetC3() * humidity + constants.GetC4() * temperature * humidity + constants.GetC5() * temperature * temperature + constants.GetC6() * humidity * humidity + constants.GetC7() * temperature * temperature * humidity + constants.GetC8() * temperature * humidity * humidity + constants.GetC9() * temperature * temperature * humidity * humidity;  }
+
+        public double CalculateWindChill() { if (GetTemperature() <= 10 && GetWindSpeed() > 4.8) return 13.12 + 0.6215 * GetTemperature() - 11.37 * Math.Pow(GetWindSpeed(), 0.16) + 0.3965 * GetTemperature() * Math.Pow(GetWindSpeed(), 0.16);
+            else return 0;
+        }
+
+        public Weather FindWeatherWithLargestWindchill(Weather[] weathers) 
+        {
+            int maxIndex = 0;
+            for(int i = 1; i < weathers.Length; i++)
+            {
+                if (weathers[i].CalculateWindChill() > weathers[maxIndex].CalculateWindChill()) maxIndex = i;
+            }
+            return weathers[maxIndex];
+        }
+    }
+}
